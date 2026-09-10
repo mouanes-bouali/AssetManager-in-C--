083b8cebd742959e09720b421964dd4af1185a27
Mouenis <mouenis2@gmail.com>
2026-09-10 15:18:51 +0100

    feat(core): add implementation file

---
+++ AssetManager/src/core/impl.cpp
@@ -0,0 +1,12 @@
+// ──────────────────────────────────────────────────────────────
+// Implementation of third-party libraries
+// This is the ONLY .cpp file you need.
+// ──────────────────────────────────────────────────────────────
+
+// stb_image implementation
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// tinyobjloader implementation
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
